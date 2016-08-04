f6d627b58cab4032a2e3c8e3d68e1f0545ba6bdb
dschaub <dan@danschaub.com>
2016-08-04 16:58:28 -0400

    Add test display manager

--- clock.rb
+++ clock.rb
@@ -1,94 +1,25 @@
 require './arrival_time_finder'
-require 'ws2812'
+require './display_manager'
 
-class DisplayManager
-  attr_reader :display
-  delegate :clear, to: :display
+require 'optparse'
 
-  def initialize(options = {})
-    @display = Ws2812::UnicornHAT.new
-    @display.rotation = options.delete(:rotation) || 180
-    @display.brightness = options.delete(:brightness) || 10
+options = {}
+OptionParser.new do |config|
+  config.banner = "Usage: clock.rb -s STOP_ID [-t]"
+
+  # TODO: add station ID and show north/south
+  config.on('-s', '--stop STOP', 'Stop ID') do |stop|
+    options[:stop_id] = stop
   end
 
-  def show_times(times, now)
-    planner = TrainDisplayPlanner.new
-
-    times.sort.each do |t|
-      remaining = ((t - now) / 60).round
-      next if remaining < 0
-      planner.add_arrival_in(remaining)
-    end
-
-    show_grid(planner.to_grid)
+  config.on('-t', '--test', 'Test mode') do |test|
+    options[:test] = test
   end
-
-  def show_grid(grid)
-    display.clear(false)
-
-    grid.each_with_index do |row, y|
-      row.each_with_index do |color, x|
-        display[x, y] = color
-      end
-    end
-
-    display.show
-  end
-
-  def show_error
-    display.clear(false)
-    color = Ws2812::Color.new(0, 0, 0x66)
-    display[0, 0] = color
-    display[0, 7] = color
-    display[7, 0] = color
-    display[7, 7] = color
-    display.show
-  end
-
-  class TrainDisplayPlanner
-    ROWS = 8
-    COLS = 8
-
-    RED = Ws2812::Color.new(0xEE, 0x35, 0x2E)
-    WHITE = Ws2812::Color.new(0xFF, 0xFF, 0xFF)
-    OFF = Ws2812::Color.new(0, 0, 0)
-
-    def initialize
-      @x = 0
-      @y = 0
-      @showing_minutes = 0
-      @train_pixels = []
-    end
-
-    def add_arrival_in(minutes)
-      relative_arrival = minutes - @showing_minutes
-      relative_arrival.times { @train_pixels << RED } if relative_arrival > 0
-      @train_pixels << WHITE
-      @showing_minutes += relative_arrival
-    end
-
-    def to_grid
-      0.upto(ROWS * COLS - 1).reduce([]) do |memo, i|
-        color = @train_pixels[i]
-        col = i % COLS
-        row = i / COLS
-        col = COLS - 1 - col if row % 2 == 1
-
-        memo[row] = [] unless memo[row]
-        memo[row][col] = color || OFF
-
-        memo
-      end
-    end
-  end
-end
+end.parse!
 
 if __FILE__ == $0
-  # TODO: make this a station ID so we can show uptown/downtown
-  stop_id = ARGV[0]
-  finder = ArrivalTimeFinder.new(stop_id)
-
-  manager = DisplayManager.new
+  finder = ArrivalTimeFinder.new(options[:stop_id])
+  manager = DisplayManager.new(test: options[:test])
 
   begin
     loop do
---
+++ display_manager.rb
@@ -0,0 +1,103 @@
+class DisplayManager
+  attr_reader :display
+  delegate :clear, to: :display
+
+  ROWS = 8
+  COLS = 8
+
+  RED = [0xEE, 0x35, 0x2E]
+  WHITE = [0xFF, 0xFF, 0xFF]
+  OFF = [0, 0, 0]
+  ERROR = [0, 0, 0x66]
+
+  def initialize(options = {})
+    @test = options[:test].nil? ? false : options[:test]
+
+    display.rotation = options.delete(:rotation) || 180
+    display.brightness = options.delete(:brightness) || 10
+  end
+
+  def show_times(times, now)
+    arranger = TrainArranger.new
+
+    times.sort.each do |t|
+      remaining = ((t - now) / 60).round
+      next if remaining < 0
+      arranger.add_arrival_in(remaining)
+    end
+
+    show_grid(arranger.to_grid)
+  end
+
+  def show_grid(grid)
+    display.clear(false)
+
+    grid.each_with_index do |row, y|
+      row.each_with_index do |color, x|
+        display[x, y] = _color(color)
+      end
+    end
+
+    display.show
+  end
+
+  def show_error
+    display.clear(false)
+    display[0, 0] = _color(ERROR)
+    display[0, ROWS - 1] = _color(ERROR)
+    display[COLS - 1, 0] = _color(ERROR)
+    display[COLS - 1, ROWS - 1] = _color(ERROR)
+    display.show
+  end
+
+  private
+
+  def display
+    if @test
+      require './terminal_display'
+      @display ||= TerminalDisplay.new
+    else
+      require 'ws2812'
+      @display ||= Ws2812::UnicornHAT.new
+    end
+  end
+
+  def _color(rgb)
+    if @test
+      TerminalDisplay::Color.new(*rgb)
+    else
+      Ws2812::Color.new(*rgb)
+    end
+  end
+
+  class TrainArranger
+    def initialize
+      @x = 0
+      @y = 0
+      @showing_minutes = 0
+      @train_pixels = []
+    end
+
+    def add_arrival_in(minutes)
+      relative_arrival = minutes - @showing_minutes
+      relative_arrival.times { @train_pixels << RED } if relative_arrival > 0
+      @train_pixels << WHITE
+      @showing_minutes += relative_arrival
+    end
+
+    def to_grid
+      0.upto(ROWS * COLS - 1).reduce([]) do |memo, i|
+        color = @train_pixels[i]
+        col = i % COLS
+        row = i / COLS
+        col = COLS - 1 - col if row % 2 == 1
+
+        memo[row] = [] unless memo[row]
+        memo[row][col] = color || OFF
+
+        memo
+      end
+    end
+  end
+end
+
